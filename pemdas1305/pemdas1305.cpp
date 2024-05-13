bc46dd6b3a6dbf2ab2984f142a2dff9b0ba96b8f
Ekoramadhan13 <enugraha411@gmail.com>
2024-05-13 10:30:31 +0700

    menambahkan class mahasiswa

---
+++ pemdas1305/pemdas1305.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+public:
+    static int nim;
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+    mahasiswa(string pnama)
+    {
+        nama = pnama;
+        setID();
+    }
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
